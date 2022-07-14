60a9c234363a813d410ab38062188edb11a09d29
alvaro-chiri <alvaro.chirinos@yahoo.com>
2022-07-14 22:30:05 +0000

    done

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -5,7 +5,7 @@ export function Footer() {
 		<div
 			className="container-fluid bg-dark text-light d-flex justify-content-center fixed-bottom"
 			style={{ padding: "1rem" }}>
-			<span> Made with ☕ by <a href="https://hurtadojose.com" title="Made by Jose Hurtado" target="_blank">Jose Hurtado</a>.</span>
+			
 		</div>
 	);
 }
